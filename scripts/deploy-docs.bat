d11d21a9c13589946545056db6b8c2a98d5392c3
teidesu <me@tei.su>
2020-05-28 20:56:46 +0300

    fix(docs): changed url format in deplot scripts

--- scripts/deploy-docs.bat
+++ scripts/deploy-docs.bat
@@ -8,6 +8,6 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m deploy
-git push -f git@github.com:plashiki/plashiki.github.io.git master
+git push -f https://github.com/plashiki/plashiki.github.io.git master
 
 cd %cwd%
--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -7,5 +7,5 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:plashiki/plashiki.github.io.git master
+git push -f https://github.com/plashiki/plashiki.github.io.git master
 cd -
